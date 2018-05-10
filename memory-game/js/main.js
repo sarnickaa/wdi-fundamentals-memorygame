40b33d1f5cdc0eb4e9e0b1bd843f43d0b2c71476
Aleksandra Sarnicka <aleksandrasarnicka@aleksandra-sarnickas-macbook.local>
2018-05-10 16:19:07 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,21 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+cardsInPlay.push (cardOne);
 
-console.log("User flipped " + cardThree);
+var cardTwo = cards[2];
+cardsInPlay.push (cardTwo);
+
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You Found A Match");
+	}
+	else { alert("Sorry, Try Again!");}
+}
+
+
+
