2f5b6dee85c8e7d0b3bdda4d57033491cfaf065c
Aleksandra Sarnicka <aleksandrasarnicka@aleksandra-sarnickas-macbook.local>
2018-05-11 15:52:15 -0400

    unit 9 assignment changes

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,20 +2,35 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push (cardOne);
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+//console.log("You found a match!");
+alert("You Found A Match!");
+ } else {
+ //console.log("Sorry, try again.");
+alert("Sorry! Try Again");
+ }
+ };
 
-var cardTwo = cards[2];
-cardsInPlay.push (cardTwo);
-
-console.log("User flipped " + cardTwo)
+var flipCard = function (cardId) {
 
 if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You Found A Match");
-	}
+
+	if (cards[0] === cards[1]) {
+		alert("You Found A Match");}
 	else { alert("Sorry, Try Again!");}
 }
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
 
