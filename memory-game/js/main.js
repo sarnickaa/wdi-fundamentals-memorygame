e1991c1d843d1294291a0f396ef93ae34e24b82b
Aleksandra Sarnicka <aleksandrasarnicka@aleksandra-sarnickas-macbook.local>
2018-05-14 13:18:09 -0400

    created new game logic-objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,28 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "heart",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "heart",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -20,13 +43,17 @@ if (cardsInPlay.length === 2) {
 		alert("You Found A Match");}
 	else { alert("Sorry, Try Again!");}
 }
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+console.log("User flipped " + cards[cardId].cardImage);
+console.log("User flipped " + cards[cardId].suit);
 };
 
 flipCard(0);
 flipCard(2);
+
+
 
 
 
