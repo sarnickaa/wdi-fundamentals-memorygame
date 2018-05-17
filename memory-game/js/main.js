e11d0d2e09db52c8f88733c8c4698e4597ca7c0d
Aleksandra Sarnicka <aleksandrasarnicka@aleksandra-sarnickas-macbook.local>
2018-05-17 12:47:28 -0400

    created game logic_final

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -25,6 +25,26 @@ var cards = [
 
 var cardsInPlay = [];
 
+
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+if (cardsInPlay.length === 2); {
+
+	//if (cards[0] === cards[1]) {
+		//alert("You Found A Match");}
+	//else { alert("Sorry, Try Again!");}
+//}
+//console.log("User flipped " + cards[cardId].rank);
+
+checkForMatch();}
+//console.log("User flipped " + cards[cardId].cardImage);
+//console.log("User flipped " + cards[cardId].suit);
+};
+
 var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 //console.log("You found a match!");
@@ -35,23 +55,43 @@ alert("Sorry! Try Again");
  }
  };
 
-var flipCard = function (cardId) {
-
-if (cardsInPlay.length === 2) {
-
-	if (cards[0] === cards[1]) {
-		alert("You Found A Match");}
-	else { alert("Sorry, Try Again!");}
-}
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-checkForMatch();
-console.log("User flipped " + cards[cardId].cardImage);
-console.log("User flipped " + cards[cardId].suit);
+var createBoard = function () {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 };
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+var button = document.getElementById('refresh');
+
+function refreshPage () {
+	location.reload();
+};
+
+button.addEventListener('click', refreshPage);
+
+
+var scoreButton = document.getElementById('score');
+
+function usersScore () {
+var userInput = document.getElementById('winorlose').value;
+//var result = userInput.value;
+
+var newListItem = document.createElement('li');
+newListItem.innerHTML = userInput;
+
+document.getElementById('scorelist').appendChild(newListItem);
+};
+
+scoreButton.addEventListener('click', usersScore);
+
+
+
 
 
 
